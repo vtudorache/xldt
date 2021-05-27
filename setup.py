c0561716976afb026eb4907bc557be24a11be6fa
Vlad Tudorache <tudorache.vlad@gmail.com>
2021-05-27 23:21:56 +0200

    Update keywords in PyPI setup

--- setup.py
+++ setup.py
@@ -5,8 +5,10 @@ xldt_description = None
 with open("README.md", "r", encoding="utf-8") as f:
     xldt_description = f.read()
 
+KEYWORDS = ["python", "excel", "date", "time"]
+
 setuptools.setup(name="xldt",
-        version="0.1.0",
+        version="0.1.1",
         author="Vlad Tudorache",
         author_email="tudorache.vlad@gmail.com",
         description="Excel-compatible date and time functions.",
@@ -18,6 +20,7 @@ setuptools.setup(name="xldt",
             "License :: OSI Approved :: MIT License",
             "Operating System :: OS Independent",
         ],
+        keywords = " ".join(KEYWORDS),
         package_dir={"": "src"},
         packages=setuptools.find_packages(where="src"),
         ext_modules=[setuptools.Extension("xldt._xldt", ["src/xldt.c"])],
